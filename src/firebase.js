a63eac057563a07513d536dab9c36206bcd743f1
kashifshafati <shafatkashif@gmail.com>
2021-02-26 20:21:18 +0500

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,6 @@
 import firebase from "firebase";
 
-const firebaseConfig = {
-	apiKey: "AIzaSyCVCBUX8CYudUMpkIhHfSSroq3IbKQQV_o",
-	authDomain: "tiktok-2f250.firebaseapp.com",
-	projectId: "tiktok-2f250",
-	storageBucket: "tiktok-2f250.appspot.com",
-	messagingSenderId: "689642053947",
-	appId: "1:689642053947:web:8cbefcc5a5f03e9fffd71e",
-	measurementId: "G-9L5T3PJQTF",
-};
+const firebaseConfig = {};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
